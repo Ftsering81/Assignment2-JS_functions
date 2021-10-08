53acd43741240407dba8becd32b0ff32258bce35
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2021-10-08 04:33:04 -0400

    Implemented and tested myMap

--- assignment2.js
+++ assignment2.js
@@ -53,3 +53,36 @@ console.log("forEach: ");
 arr1.forEach(isEven);
 */ 
 
+
+// MAP //
+/* 
+Description: The map method transforms an array by applying a callback function to all of its elements and building a new array 
+from the returned values. The new array will have the same length as the input array, but its content will have been mapped to 
+a new form by the function.
+
+
+@param callbackFn - Function that is called for every element of the array. Each time callbackFn executes, the returned value is added to newArray.
+callbackFn is commonly used with one argument, even though they take additional optional arguments.
+callbackFn accepts between one and three arguments: the value of the element, the index of the element(optional), the array object being traversed(optional).
+callbackFn is invoked only for indexes of the array which have assigned values (including undefined)
+
+@return value - A new array with each element being the result of the callback function.
+
+*/
+Array.prototype.myMap = function(callbackFn) {
+	let mapped = []; //array that will be returned
+	for(let i = 0; i < this.length; i++) {
+		//the value returned after callbackFn has been applied to element of the array at index i is added to the new array mapped at same index
+		mapped[i] = callbackFn(this[i], i, this); 
+	}
+	return mapped; //returns the new array which is populated with the results of the callback function being applied on every element in the calling array.
+};
+
+// TEST FOR myMap //
+/*
+arr = [1, 2, 3];
+const square = (num) => num * num; //this function returns the square of num
+console.log("myMap: ", arr.myMap(square)); //function square passed as argument for callback function
+console.log("map: ", arr.map(square));
+*/
+
